8a6260a4c7901e6c2c6c48ab215d4dfd053f9a6c
Francisco Ochoa Gonzales <linuxer41@gmail.com>
2025-07-24 14:40:30 -0400

    add max width

--- frontend/src/lib/components/layouts/Components/MainContent.svelte
+++ frontend/src/lib/components/layouts/Components/MainContent.svelte
@@ -8,6 +8,8 @@
         z-index: 1;
         height: 100%;
         min-height: 100%;
+        margin: auto;
+        max-width: 480px;
         width: 100%;
         @media screen and (max-width: 768px) {
             padding-top: 0;
